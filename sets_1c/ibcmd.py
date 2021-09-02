5514cd0424e292996ba0701454db2876b00b58b3
obartoshyk <_23uk.85@mail.ru>
2021-09-02 11:37:40 +0300

    add ibcmd add upload_sandbox

--- sets_1c/ibcmd.py
+++ sets_1c/ibcmd.py
@@ -12,12 +12,12 @@ def get_postgres_bparams(srv, base, usr, pwd):
     return bparams
 
 def get_file_bparams(pach):
-    return ["--db-path={}".format(pach)]
+    return ['--db-path={}'.format(pach)]
 
 class IbCmd(object):
     """ IbCmd server utils"""
 
-    def __init__(self, cmd_func, bparams, platform="deb", test=True):
+    def __init__(self, cmd_func="", bparams=[], platform="deb", test=True):
         super(IbCmd, self).__init__()
         sets = settings_1c.Settings()
         self.cmd_func = cmd_func
@@ -25,12 +25,13 @@ class IbCmd(object):
         self.bparams = bparams
         self.ibcmd_pach = sets.ibcmd_pach[platform]
 
-    def run(self, cmd):
-            if not self.test:
-                print("run: {}".format(cmd))
-                self.cmd_func(cmd)
-            else:
-                print("run(TEST): {}".format(cmd))
+    def run(self, cmd0, *args):
+        cmd = self.create_cmd(cmd0, *args)
+        if not self.test:
+           #print("run: {}".format(cmd))
+           self.cmd_func(cmd)
+        else:
+           print("run(TEST): {}".format(cmd))
 
     def create_cmd(self, cmd0, *args):
         return "{0} {1} {2}".format(self.ibcmd_pach, cmd0, " ".join([*self.bparams, *args]))
--- upload_sandbox_cf.py
+++ upload_sandbox_cf.py
@@ -4,22 +4,52 @@
 
 from sets_1c import ibcmd
 from sets_1c import settings_1c
+from sets_1c import connection_1c
 from sets_1c import argparse_1c
+import regibsrv
 import os
 
-parser =argparse_1c.ArgumentParser_1C("sbk", description=__doc__)
+parser = argparse_1c.ArgumentParser_1C("sbk", description=__doc__)
 parser.add_argument('-p', '--pach',
-				metavar="PACH",
-				help='local base pach',
-				nargs=1,type=str, required=True)
+                    metavar="PACH",
+                    help='local base pach',
+                    nargs=1, type=str, required=True)
+parser.add_argument('-n', '--sandbox',
+                    metavar="PACH",
+                    help='sandbox base pach',
+                    nargs=1, type=str, required=True)
 parser.decode_arg()
+sandbox = "{}/{}".format(parser.args["sandbox"][0], parser.b[0])
 
 cmdmn = ibcmd.IbCmd(
-            lambda x: os.system(x),
-            ibcmd.get_file_bparams(parser.args["pach"][0]),
-            "deb", parser.args["test"])
+    cmd_func=lambda x: os.system(x),
+    bparams=ibcmd.get_file_bparams(parser.args["pach"][0]),
+    platform="deb",
+    test=parser.args["test"])
 
-fm = settings_1c.FileManager()
-tmpf = fm.tmpf("/tmp", parser.b[0], "cf")
+tmpfile = settings_1c.FileManager().tmpf("/tmp", parser.b[0], "cf")
+cmdmn.run("infobase config save", tmpfile)
 
-cmdmn.run(cmdmn.create_cmd("config save", tmpf))
+with connection_1c.Connection(srv=parser.s[0], **parser.args) as conn:
+    if not conn.testmode:
+        ftp = conn.ssh.open_sftp()
+        print("uploading to {}".format(parser.s[0]))
+        ftp.put(tmpfile, tmpfile)
+        os.remove(tmpfile)
+    else:
+        ftp = False
+    oIbSrv = regibsrv.IbSrv(base=parser.args["base"][0],
+                            pach=parser.args["sandbox"][0],
+                            test=parser.args["test"])
+    rIbCmd = ibcmd.IbCmd(
+        cmd_func=lambda x: conn.cast(x),
+        bparams=ibcmd.get_file_bparams(sandbox),
+        platform="deb",
+        test=parser.args["test"])
+    oIbSrv.remote("stop", conn)
+    rIbCmd.run("infobase config load", tmpfile)
+    rIbCmd.run("infobase config apply", "--force")
+    oIbSrv.remote("start", conn)
+    if ftp:
+        ftp.remove(tmpfile)
+        ftp.close()
