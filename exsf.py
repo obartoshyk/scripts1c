2ae5ad8ecc98ce151c2a7ff6a53edeab49fe9db1
obartoshyk <_23uk.85@mail.ru>
2022-05-23 15:42:21 +0300

    fix exsf

--- exsf.py
+++ exsf.py
@@ -4,14 +4,14 @@ from utils_1c import localfilestorage, argparse_1c
 import os
 
 parser = argparse_1c.ArgumentParser_1C("sl", description=__doc__)
-parser.add_argument('-o', '--o_pach',
+parser.add_argument('-p', '--pach',
                     metavar="O_PACH",
                     help='/var/lib/gtmms/data/AAAA/YYYYYY/XXXXXX/DPA_5.1.pdf',
                     nargs=1, type=str, required=True)
 parser.decode_arg()
 
-if not os.path.exists(parser.args['o_pach'][0]):
+if not os.path.exists(parser.args['pach'][0]):
     localfilestorage.FileStorage(srv=parser.args["server"][0],
                                  storage_path=parser.args['gtmms'][0],
                                  trn=0,
-                                 **p.args).get(parser.args['o_pach'][0])
+                                 **parser.args).get(parser.args['pach'][0])
