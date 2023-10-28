5f2d632678a5100b551adcd63e2c8b5e0d23b548
obartoshyk <_23uk.85@mail.ru>
2023-10-28 16:45:12 +0300

    8.3.23.1912

--- repack_1cdistr.py
+++ repack_1cdistr.py
@@ -7,10 +7,10 @@ from utils_1c import settings_1c, argparse_1c
 
 def send_to_srv(sets,update_sets,srv_list, ssh_key):
 	#scp -r $pach0/$t_name.zip  root@SRV:/root/install/$t_name.zip
-	dest = "{0}/{1}.zip".format(sets.update_cat,update_sets["upd_name"])
+	dest = "{0}/{1}".format(sets.update_cat,update_sets["upd_name"])
 	cmd_list = []
 	for srv in srv_list: 
-		cmd = "scp -i {0} -r {1} root@{2}:/var/www/html/distr1c/new/{3}.zip".format(ssh_key,dest,srv,update_sets["upd_name"])
+		cmd = "scp -i {0} -r {1} root@{2}:/var/www/html/distr1c/new/{3}".format(ssh_key,dest,srv,update_sets["upd_name"])
 		cmd_list.append(cmd)
 	if sets.test_mode:
 		for cmd in cmd_list:
@@ -23,7 +23,7 @@ def repack_win(sets,update_sets):
 	"""~/8.3.18.1289/setuptc64_8_3_18_1289.rar"""
 	sourse = "{0}/{1}{2}.rar".format(sets.update_cat,update_sets['source_name'],sets.version_1C_)
 	tcat = "{0}/{1}".format(sets.update_cat,update_sets['upd_name'])
-	dest = "{0}.zip".format(tcat)
+	dest = "{0}".format(tcat)
 	if sets.test_mode:
 		print("win: {0} =~> {1} =~> {2}".format(sourse,tcat,dest))
 	else:	
@@ -39,7 +39,7 @@ def repack_osx(sets,update_sets):
 						sets.update_cat,
 						update_sets['source_name'],
 						sets.version_1C_)
-	dest = "{0}/{1}.dmg".format(sets.update_cat,update_sets["upd_name"])
+	dest = "{0}/{1}".format(sets.update_cat,update_sets["upd_name"])
 	if sets.test_mode:
 		print("osx: {0} =~> {1}".format(sourse,dest))
 	else:	
@@ -54,7 +54,7 @@ def repack_deb(sets,update_sets):
 	#sudo chmod a+r $pach0/$t_name.zip
 	sourse = "{0}/{1}{2}.tar.gz".format(sets.update_cat,update_sets['source_name'],sets.version_1C_)
 	tcat = "{0}/{1}".format(sets.update_cat,update_sets['upd_name'])
-	dest = "{0}.run".format(tcat)
+	dest = "{0}".format(tcat)
 	if sets.test_mode:
 		print("deb: {0} =~> {1} =~> {2}".format(sourse, tcat, dest))
 	else:	
